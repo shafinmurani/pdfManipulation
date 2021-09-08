87305b5deb5ce5789f2ec8900ca2a5f44b35d757
Shafin murani <shafinmurani9@gmail.com>
2021-09-08 20:40:39 +0530

    Compressing PDFs Done

---
+++ index.php
@@ -0,0 +1,140 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c8a3e8bc04.js" crossorigin="anonymous"></script>
+    <title>Pdf Converter</title>
+</head>
+
+<body>
+    <div class="Home-page">
+        <div class="header">
+
+            <div class="menubar">
+                <div class="logo">LOGO</div>
+                <div class="menus">
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a>Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header-content">
+                <div class="header-text">
+                    <h2 class="t1">Free Online</h2>
+                    <h2 class="t2">PDF Converter</h2>
+                    <hr>
+                    <p>You can convert your files to PDF, edit, unlock, split, merge PDF files, export PDF files (to Text, Image, Word, Powerpoint) and much more.</p>
+                    <a href="#services">CONVERT NOW</a>
+                </div>
+                <div class="header-image">
+                    <img src="vector_11.svg" alt="Failed to load image">
+                </div>
+            </div>            
+        </div>
+        <div class="service-detail" id="services">
+            <div class="service-head">
+                <h2>Our Tools</h2>
+            </div>
+            <div class="service-tools">
+                <div class="tool">
+                    <a href="mergepdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Merge PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="split.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Split PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="compresspdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Compress PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="pdftoword.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>PDF to Word</p>
+                </div>
+                <div class="tool">
+                    <a href="pdftopowerpoint.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>PDF to Powerpoint</p>
+                </div>
+                <div class="tool">
+                    <a href="pdftoexcel.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>PDF to Excel</p>
+                </div>
+                <div class="tool">
+                    <a href="wordtopdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Word to PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="powerpointtopdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Powerpoint to PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="exceltopdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Excel to PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="editpdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Edit PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="pdftojpg.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>PDF to JPG</p>
+                </div>
+                <div class="tool">
+                    <a href="jpgtopdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>JPG to PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="pagenumber.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Page Numbers</p>
+                </div>
+                <div class="tool">
+                    <a href="watermark.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Watermark</p>
+                </div>
+                <div class="tool">
+                    <a href="rotatepdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Rotate PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="htmltopdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>HTML to PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="unlockpdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Unlock PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="protectpdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Protect PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="organizepdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Organize PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="pdftopdfa.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>PDF to PDF/A</p>
+                </div>
+                <div class="tool">
+                    <a href="repairpdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Repair PDF</p>
+                </div>
+                <div class="tool">
+                    <a href="signpdf.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                    <p>Sign PDF</p>
+                </div>
+                
+                               
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ refinput.php
@@ -0,0 +1,48 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merge PDF</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="mergepdf.css">
+</head>
+
+<body>
+    <div class="mergepdf" style="background: #202020;">
+        <div class="header">
+
+            <div class="menubar">
+                <div class="logo">LOGO</div>
+                <div class="menus">
+                    <ul>
+                        <li><a a='/'>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a>Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="mergepdf-header">
+                <h2>Merge PDF files</h2>
+                <p>Combine PDFs in the order you want with the easiest PDF merger available.</p>
+            </div>
+            <div class="mergepdf-form">
+                <form action="" method="post" enctype="multipart/form-data">
+                <div>
+                    <input type="file" name="pdf1">
+                    <br>
+                    <button type="submit" name="save" class="btn">Upload</button>
+                </div>
+                </form>
+                <?php
+                
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
